0a64c1313647190482f840411e3df385276dcdff
Hunter-Chambers <hrchambers.333@gmail.com>
2020-04-12 14:41:30 -0500

    Worked on CS3372/Lab05/, Need to update ARP Table

--- CS3372/Lab05/hc998658_lab5.py
+++ CS3372/Lab05/hc998658_lab5.py
@@ -3,25 +3,33 @@
 from scapy.all import *
 
 def main():
-    packets = sniff(filter="icmp", count = 2)
+    arp_update = Ether()/ARP()
 
-    for packet in packets:
-        print(packet.show())
-        #print(packet.summary())
-        print("-"*60)
+    packets = sniff(filter="icmp", count = 1)
+    packet = packets[0]
 
-    r_dst = packets[0][IP].src
-    r_src = packets[0][IP].dst
-    r_seq = packets[0][ICMP].seq
-    r_raw = packets[0][Raw].load
-    ping_response = Ether()/IP(dst=r_dst, src=r_src, id=0)/ICMP(type=0, seq=r_seq, id=1)/Raw(load=r_raw)
-    ping_response[IP].len = 60
-    ping_response[Ether].src = packets[0][Ether].dst
-    ping_response[Ether].dst = packets[0][Ether].src
+    arp_update[Ether].dst = packet[Ether].src
+    arp_update[ARP].psrc = "192.168.11.1"
+    arp_update[ARP].pdst = packet[IP].src
 
-    sendp(ping_response)
-    print(ping_response.show())
-    #print(ping_response.summary())
+    sendp(arp_update)
+    #print(arp_update.show())
+
+    '''
+    icmp_response = Ether()/IP()/ICMP()/Raw()
+    icmp_response[Ether].src = packet[Ether].dst
+    icmp_response[Ether].dst = packet[Ether].src
+    icmp_response[IP].src = packet[IP].dst
+    icmp_response[IP].dst = packet[IP].src
+    icmp_response[IP].id = 0
+    icmp_response[IP].len = 60
+    icmp_response[ICMP].type = 0
+    icmp_response[ICMP].seq = packet[ICMP].seq
+    icmp_response[ICMP].id = 1
+    icmp_response[Raw].load = packet[Raw].load
+
+    sendp(icmp_response)
+    '''
 
 if __name__ == "__main__":
     main()
