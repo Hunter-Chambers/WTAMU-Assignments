ce5a2e7ab5d648e2dc0209e82eac6a759a70ce14
Hunter Chambers <hc998658@thor.wtstudents.wtamu.edu>
2020-09-22 07:16:18 -0500

    Downloaded Lab01. NOT FINISHED

---
+++ CS2336/Lab01/Java/Solution/Statexam.java
@@ -0,0 +1,342 @@
+// FILE:  Statexam.java
+// This program calls five test function to test the Statistician class.
+// Maximum number of points from this program is 100.
+//
+public class Statexam {
+
+    public static void main( String [] args ) {
+        int value = 0;
+        int result= 0;
+
+        System.out.printf("Running Statistician tests:\n");
+
+        System.out.printf("TEST 1:\n");
+        System.out.printf("Testing nextNumber, reset, length, sum, and mean (51 points).\n");
+        result = test1( );
+        value += result;
+        if ( result > 0 ) System.out.printf("Test 1 passed.\n\n");
+        else System.out.printf("Test 1 failed.\n\n");
+
+        System.out.printf("TEST 2:\n");
+        System.out.printf("Testing smallest and largest methods (10 points).\n");
+        result = test2( );
+        value += result;
+        if ( result > 0 ) System.out.printf("Test 2 passed.\n\n");
+        else System.out.printf("Test 2 failed.\n\n");
+
+        System.out.printf("Test 5:\n");
+        System.out.printf("Testing equals method (12 points).\n");
+        result = test5( );
+        value += result;
+        if ( result > 0 ) System.out.printf("Test 5 passed.\n\n");
+        else System.out.printf("Test 5 failed.\n\n");
+
+        System.out.printf("Test 3:\n");
+        System.out.printf("Testing union method (10 points).\n");
+        result = test3( );
+        value += result;
+        if ( result > 0 ) System.out.printf("Test 3 passed.\n\n");
+        else System.out.printf("Test 3 failed.\n\n");
+
+        System.out.printf("Test 4:\n");
+        System.out.printf("Testing scale method (17 points).\n");
+        result = test4( );
+        value += result;
+        if ( result > 0 ) System.out.printf("Test 4 passed.\n\n");
+        else System.out.printf("Test 4 failed.\n\n");
+
+        System.out.printf("If you submit the Statistician to Prof. Haiduk now\n" +
+                          "this part of the grade will be %d points out of 100\n", value);
+        System.exit(0);
+
+    }
+
+    public static boolean close( double a, double b ) {
+        double EPSILON = 1e-5;
+        return ( Math.abs(a-b) < EPSILON );
+    }
+
+    public static int test1( ) {
+        // Test function for basic Statistician functions.
+        // Returns 51 if everything goes okay; otherwise, return 0.
+        
+        Statistician stat1 = new Statistician(); 
+        Statistician stat2 = new Statistician( );
+        int points = 0;
+
+        double checker = 0.0;
+        double checkerSq = 0.0;
+        if ( stat1.length( ) != 0 || stat2.length( ) != 0 ) return 0;
+        points += 2;
+        System.out.printf("Test1 -- part 1 OK\n");
+
+        if ( stat1.sumX( ) != 0 || stat2.sumX( ) != 0 ) return 0;
+        points += 2;
+        System.out.printf("Test1 -- part 2 OK\n");
+
+        if ( !Double.isNaN( stat1.mean( ) ) ) return 0;
+        points += 3;
+
+        int i;
+        for (i = 1; i <= 10000; i++) {
+            stat1.nextNumber(i);
+            checker += i;
+            checkerSq += (i*i);
+        }
+
+        if ( stat2.length( ) != 0 || stat2.sumX( ) != 0 ) return 0;
+        System.out.printf("Test1 -- part 3 OK\n");
+        points += 2;
+
+        if ( stat1.length( ) != 10000 ) return 0;
+        System.out.printf("Test1 -- part 4 OK\n");
+        points += 4;
+
+        if ( !close(stat1.sumX( ), checker ) ) return 0;
+        System.out.printf("Test1 -- part 5 OK\n");
+        points += 4;
+
+        if ( !close(stat1.mean( ), checker/10000.0) ) return 0;
+        System.out.printf("Test1 -- part 6 OK\n");
+        points += 5;
+
+        double mean = checker/10000.0;
+        double stdDev = Math.sqrt( checkerSq/10000.0 - mean*mean );
+
+        if ( !close(stat1.stdDev(), stdDev ) ) return 0;
+        System.out.printf("Test1 -- part 7 OK\n");
+        points += 6;
+
+        // Reset and then retest everything
+        stat1.reset( ); stat2.reset( ); checker = 0.0; checkerSq = 0.0;
+
+        if ( stat1.length( ) != 0 || stat2.length( ) != 0 ) return 0;
+        System.out.printf("Test1 -- part 8 OK\n");
+        points += 2;
+
+        if ( stat1.sumX( ) != 0 || stat2.sumX( ) != 0 ) return 0;
+        System.out.printf("Test1 -- part 9 OK\n");
+        points += 2;
+
+        if ( !Double.isNaN( stat1.mean( ) ) ) return 0;
+        System.out.printf("Test1 -- part 10 OK\n");
+        points += 4;
+
+        for (i = 1; i <= 10000; i++) {
+            stat1.nextNumber(i);
+            checker += i;
+            checkerSq += (i*i);
+        }
+
+        if ( stat2.length( ) != 0 || stat2.sumX( ) != 0 ) return 0;
+        System.out.printf("Test1 -- part 11 OK\n");
+        points += 2;
+
+        if ( stat1.length( ) != 10000 ) return 0;
+        System.out.printf("Test1 -- part 12 OK\n");
+        points += 2;
+
+        if ( !close(stat1.sumX( ), checker) ) return 0;
+        System.out.printf("Test1 -- part 13 OK\n");
+        points += 2;
+
+        if ( !close(stat1.mean( ), checker/10000.0) ) return 0;
+        System.out.printf("Test1 -- part 14 OK\n");
+        points += 3;
+
+        mean = checker/10000.0;
+        stdDev = Math.sqrt( checkerSq/10000.0 - mean*mean );
+        if ( !close(stat1.stdDev(), stdDev) ) return 0;
+        System.out.printf("Test1 -- part 15 OK\n");
+        points += 6;
+
+        System.out.printf("points from this test %d\n", points);
+        return points;
+    }
+
+    public static int test2( ) {
+        // Test function for smallest/largest Statistician functions.
+        // Returns 10 if everything goes okay; otherwise returns 0;
+
+        Statistician s = new Statistician(); Statistician t = new Statistician();
+        Statistician u = new Statistician();
+
+        int points = 0;
+
+        if ( s.length( ) != 0 || t.length( ) != 0 ) return 0;
+        points += 1;
+        if ( s.sumX( ) != 0 || t.sumX( ) != 0 ) return 0;
+        points += 1;
+
+        if ( !Double.isNaN( s.smallest( ) ) ) return 0;
+        points += 1;
+
+        if ( !Double.isNaN( s.largest( ) ) ) return 0;
+        points += 1;
+
+        double r = 1.39804e-76;
+        r = 1/r;
+        s.nextNumber( r );
+        if ( (s.smallest( ) != r ) || (s.largest( ) != r) ) return 0;
+        points += 2;
+
+        r *= -1;
+        t.nextNumber(r);
+        if ( (t.smallest( ) != r) || (t.largest( ) != r) ) return 0;
+        points += 2;
+
+        u.nextNumber( 100 ); u.nextNumber( -1 ); u.nextNumber( 101 ); u.nextNumber( 3 );
+        if ( (u.smallest( ) != -1) || (u.largest( ) != 101) ) return 0;
+        points += 2;
+
+        System.out.printf("points from this test %d\n", points);
+        return points;
+    }
+
+    public static int test3( ) {
+        // Test function for the union and equals method of the Statistician
+        // Return 10 if everything is okay; otherwise, returns 0.
+
+        Statistician s, t, u, v;
+        s = new Statistician(); t = new Statistician();
+        u = new Statistician(); v = new Statistician();
+
+        int points = 0;
+
+        if ( s.length( ) != 0 || 
+             t.length( ) != 0 || 
+             u.length( ) != 0 || 
+             v.length( ) != 0 ) return 0;
+        if ( s.sumX( ) != 0 ||
+             t.sumX( ) != 0 ||
+             u.sumX( ) != 0 ||
+             v.sumX( ) != 0 ) return 0;
+
+        t.nextNumber( 5 );
+        u.nextNumber( 0 ); u.nextNumber( 10 ); u.nextNumber( 10 ); u.nextNumber( 20 );
+
+        v = Statistician.union( s, s );
+        if ( v.length( ) != 0 || v.sumX( ) != 0 ) return 0;
+        points += 2;
+        //System.out.printf("Success with t = union( s, s)\n");
+
+        v = Statistician.union( s, u );
+        if ( !u.equals( v ) ) return 0;
+        points += 2;
+        //System.out.printf("Success with v = union( s, u)\n");
+
+        v = Statistician.union( t, s );
+        if ( !t.equals( v ) ) return 0;
+        points += 2;
+        //System.out.printf("Success with v = union( t, s)\n");
+
+        v = Statistician.union( t, u );
+        if ( v.length( ) != 5 ) return 0;
+        if ( !close( v.sumX( ), 45 ) ) return 0;
+        if ( v.smallest( ) != 0 ) return 0;
+        if ( v.largest( ) != 20 ) return 0;
+        if ( !close( v.mean( ), 45.0/5 ) ) return 0;
+        points += 2;
+        //System.out.printf("Success with v = union( t, u)\n");
+
+        v = Statistician.union( v, t );
+        if ( v.length() != 6 ) return 0;
+        if ( !close( v.sumX( ), 50 ) ) return 0;
+        if ( v.smallest( ) != 0 ) return 0;
+        if ( v.largest( ) != 20 ) return 0;
+        if ( !close( v.mean( ), 50.0/6 ) ) return 0;
+        points += 2;
+        //System.out.printf("Success with v = union( v, t)\n");
+
+        System.out.printf("points from this test %d\n", points);
+        return points;
+    }
+
+    public static int test4( ) {
+        // Test function for the Statistician scale method
+        // Returns 17 if everything goes okay; otherwise, returns 0.
+
+        Statistician s, t, u;
+        s = new Statistician(); t = new Statistician(); u = new Statistician();
+        int points = 0;
+
+        if ( s.length( ) != 0 || 
+             t.length( ) != 0 || 
+             u.length( ) != 0 ) return 0;
+        if ( s.sumX( ) != 0 ||
+             t.sumX( ) != 0 ||
+             u.sumX( ) != 0  ) return 0;
+
+        u.nextNumber( 0 ); u.nextNumber( 10 ); u.nextNumber( 10 ); u.nextNumber( 20 );
+
+        s = Statistician.scale( 2, u );
+        if ( s.length( ) != 4 ) return 0;
+        if ( !close( s.sumX( ), 80 ) ) return 0;
+        if ( s.smallest( ) != 0 ) return 0;
+        if ( s.largest( ) != 40 ) return 0;
+        if ( !close( s.mean( ), 80.0/4 ) )return 0;
+        points += 2;
+
+        s = Statistician.scale( -2, u );
+        if ( s.length( ) != 4 ) return 0;
+        if ( !close( s.sumX( ), -80 ) ) return 0;
+        if ( s.smallest( ) != -40) return 0;
+        if ( s.largest( ) != 0 ) return 0;
+        if ( !close( s.mean( ), -80.0/4 ) )return 0;
+        points += 11;
+
+        s = Statistician.scale( 0, u );
+        if ( s.length( ) != 4 ) return 0;
+        if ( !close( s.sumX( ), 0 ) ) return 0;
+        if ( s.smallest( ) != 0) return 0;
+        if ( s.largest( ) != 0 ) return 0;
+        if ( !close( s.mean( ), 0 ) )return 0;
+        points += 2;
+
+        s = Statistician.scale( 10, t );
+        if ( s.length( ) != 0 ) return 0;
+        if ( !close( s.sumX( ), 0 ) ) return 0;
+        points += 2;
+
+        System.out.printf("points from this test %d\n", points);
+        return points;
+    }
+
+    public static int test5( ) {
+        // Test function for the equals method of the Statistician
+        // Returns 12 if everything is okay; otherwise, returns 0.
+        Statistician s, t, u, v, w, x;
+        int points = 0;
+
+        s = new Statistician(); t = new Statistician(); u = new Statistician();
+        v = new Statistician(); w = new Statistician(); x = new Statistician();
+
+        t.nextNumber( 10 );
+        u.nextNumber( 0 ); u.nextNumber( 10 ); u.nextNumber( 10 ); u.nextNumber( 20 );
+        v.nextNumber( 5 ); v.nextNumber( 0 ); v.nextNumber( 20 ); v.nextNumber( 15 );
+        w.nextNumber( 0 );
+        x.nextNumber( 0 ); x.nextNumber( 0 );
+
+        if ( !s.equals(s) ) return 0;
+        points += 2;
+
+        if ( s.equals(t) ) return 0;
+        points += 2;
+
+        if ( t.equals(s) ) return 0;
+        points += 2;
+
+        if ( u.equals(t) ) return 0;
+        points += 2;
+
+        if ( u.equals(v) ) return 0;
+        points += 2;
+
+        if ( w.equals(x) ) return 0;
+        points += 2;
+
+        System.out.printf("points from this test %d\n", points);
+        return points;
+    }
+
+}
---
+++ CS2336/Lab01/Java/Solution/Statistician.java
@@ -0,0 +1,343 @@
+// File: Statistician.java
+// This is an assignment for students to complete after reading Chapter 2 of
+// "Data Structures and Other Objects Using Java" by Michael Main.
+
+import java.util.Objects;
+import static java.lang.Math.sqrt;
+
+
+/******************************************************************************
+* This class is a homework assignment designed to help students begin
+* their transition from Python into Java --
+*
+* A <CODE>Statistician</CODE> keeps track of statistics about a sequence of
+* double numbers.<br>
+*
+* @version
+*   September 2020  
+******************************************************************************/
+public class Statistician {   
+
+   //  Here we define the "instance" variables that hold
+   //  the "state" information for objects of this class.
+   //  We must manage those "instance" variables appropriately
+   //  since we are obligated to report to a user such "statistics" 
+   //  as mean, sumX, largest, smallest, length and standard deviation. 
+   //  Note that we need NOT save all the values
+   //
+   //  Class invariant:  the attributes/instance variables N, sumX,
+   //  sumXsq, largest and smallest are used to derive the statistics
+   //  and define the state of a statistician object
+
+    private int    N;        // How many numbers given to statistician
+    private double sumX;     // Sum of all the numbers given to statistician
+    private double sumXsq;   // Sum of the squares of all numbers processed
+    private double smallest; // The smallest number given to statistician
+    private double largest;  // The largest number given to statistician
+
+
+   /**
+   * Initialize a new Statistician.
+   * <b>Postcondition:</b><br>
+   *   This Statistician is newly initialized and has not yet been given any 
+   *   numbers.
+   **/   
+   public Statistician( ) {
+      this.N        = 0;
+      this.sumX     = 0.0;
+      this.sumXsq   = 0.0;
+      this.smallest = +Double.MAX_VALUE;
+      this.largest  = -Double.MAX_VALUE;
+   } 
+
+   /**
+   * Initialize a new Statistician as a copy of another Statistician.<br><br>
+   * @param other
+   *     other is a non-null reference to another Statisticiana<br><br>
+   * <b>Precondition:</b><br>
+   *   Other is not null.  If it is, then throw NullPointerException<br><br>
+   * <b>Postcondition:</b><br>
+   *   This Statistician is newly initialized and has been given attributes
+   *   equal to those of other
+   * @throws NullPointerException
+   **/   
+   public Statistician( Statistician other ) {
+       if ( other == null )
+           throw new NullPointerException("other cannot be null");
+
+       this.N = other.N;
+      // Student implementation -- initialize all remaining attributes
+      // of "this" object with the values of the "other" object
+   }  
+
+
+   /**
+   * Compare this <CODE>Statistician</CODE> to another object for equality.
+   * @param obj
+   *   an object with which this <CODE>Statistician</CODE> will be compared
+   * @return
+   *   A return value of <CODE>true</CODE> indicates that 
+   *   <CODE>obj</CODE> refers to a 
+   *   <CODE>Statistican</CODE> object with the same length, sum,
+   *   sum of all numbers squared, smallest and largest as this 
+   *   <CODE>Statistician</CODE>. Otherwise the return value is 
+   *   <CODE>false</CODE>.<br>
+   * <b>Note:</b><br>
+   *   If <CODE>obj</CODE> is null or does not refer to a 
+   *   <CODE>Statistician</CODE> object, then the return is 
+   *   <CODE>false</CODE>.
+   **/  
+   @Override
+   public boolean equals(Object obj) {
+      if ( obj == null ) return false;
+      if ( !(obj instanceof Statistician) ) return false;
+
+      Statistician temp = (Statistician) obj;
+      if ( this == temp ) return true; //Are we comparing with ourself
+      if ( this.length( ) == 0 && temp.length( ) == 0 ) return true;
+      // NOTE:  if two objects are equal, their hashCodes are equal
+      if ( this.hashCode() != temp.hashCode() ) return false;
+
+      // The student's code will remove the following return and replace
+      // it with appropriate logic that compares each and every attribute
+      // of this with attributes of temp
+      
+      return false;
+   } 
+   
+   /**
+    * Return a hashcode that is unique to an instance of
+    * class Statistician based on the value of its 
+    * attributes.
+    * @return
+    *   an int hash code that is unique to an object of
+    *   class Statistician by combining the hash codes
+    *   of all its attributes
+    **/
+   @Override
+   public int hashCode() {
+       return Objects.hash( this.N,
+                            this.sumX,
+                            this.sumXsq,
+                            this.smallest,
+                            this.largest 
+                          );
+   }
+   
+   /**
+   * Determine how many numbers have been given to this Statistician.
+   * @return
+   *   the N of how many numbers have been given to this Statistician
+   *   since it was initialized or reinitialized <br>
+   * <b>Note:</b><br>
+   *   Giving a Statistician more than
+   *   <CODE>Integer.MAX_VALUE</CODE> numbers, will
+   *   cause failure with an arithmetic overflow.
+   **/ 
+   public int length( ) {
+      return this.N;
+   }
+
+   /**
+   * Determine the largest number that has been given 
+   * to this Statistician.
+   * @return
+   *   the largest number that has been given to this 
+   *   Statistician since it was initialized or reinitialized<br>
+   * <b>Note:</b><br>
+   *   If <CODE>length()</CODE> is zero, then the return from this method
+   *   is <CODE>Double.NaN</CODE>.
+   **/ 
+   public double largest( ) {
+      // The student's code will replace the following return statement with
+      // logic that matches specifications.
+      return 0;
+   }
+
+   /**
+   * Determine the arithmetic average of all the numbers that have been given 
+   * to this Statistician.
+   * @return
+   *   the arithmetic mean of all the number that have been given to this 
+   *   Statistician
+   *   since it was initialized or reinitialized<br>
+   * <b>Note:</b><br>
+   *   If this Statistician has been given more than
+   *   <CODE>Integer.MAX_VALUE</CODE> numbers, then this method fails
+   *   because of arithmetic overflow.
+   *   If <CODE>length()</CODE> is zero, then the return from this method
+   *   is <CODE>Double.NaN</CODE>.
+   *   If <CODE>sum()</CODE> exceeds the bounds of double numbers, then the 
+   *   return from this method may be 
+   *   <CODE>Double.POSITIVE_INFINITY</CODE> or
+   *   <CODE>Double.NEGATIVE_INFINITY</CODE>.
+   **/ 
+   public double mean( ) {
+      // The student's code will replace the following return statement with
+      // code that matches specifications
+      return 0;
+   }
+
+
+   /**
+   * Give a new number to this Statistician. 
+   * @param number
+   *   the new number that is being given to this Statisticiana<br>
+   * <b>Postcondition:</b><br>
+   *   The specified number has been given to this Statistician and
+   *   it will be included in any subsequent statistics
+   **/
+   public void nextNumber(double number) {
+      // Student implementation -- must update all the
+      // instance variables
+      // NOTE:  if number is the first number given to the statistician,
+      //        then number is both the largest and smallest seen so far.
+
+   }
+   
+   /**
+   * Reset this Statistician to state with no numbers
+   * in the sequence. <br>
+   *
+   * <b>Postcondition:</b><br>
+   *   This Statistician is reinitialized as if it has never been given any 
+   *   numbers
+   **/
+   public void reset( ) {
+      // Student implementation.
+   }
+
+   /**
+    * Create a new Statistician whose state is scaled by parameter scale--
+    * such that "statistics" other than length() are multiplied by scale.
+    * <b>Postcondition:</b><br>
+    *   The returned Statistician is a scaled copy of parameter s which 
+    *   is not modified.
+    * @param scale
+    *   a double scale factor
+    * @param s
+    *   a reference to a Statistician
+    * @return
+    *   a reference to a new Statiscian that is a scaled copy of parameter s
+    **/
+   public static Statistician scale(double scale, Statistician s) {
+       Statistician temp = new Statistician(s);
+
+      // Student implementation follows
+      
+       return temp;
+   }
+
+   /**
+   * Determine the smallest number that has been given 
+   * to this Statistician.
+   * @return
+   *   the smallest number that has been given to this 
+   *   Statistician since it was initialized or reinitialized<br>
+   * <b>Note:</b><br>
+   *   If <CODE>length()</CODE> is zero, then the return from this method
+   *   is <CODE>Double.NaN</CODE>.
+   **/ 
+   public double smallest( ) {
+      // The student's code will replace the following return statement with
+      // code that matches specifications
+      return 0;
+   }
+
+   /**
+   * Determine the standard deviation of the stream of numbers
+   * given to this statistician.
+   * <b>Postcondition:</b><br>
+   *   State of this statistician is NOT altered
+   *
+   * @return
+   *    The standard deviation of the stream of numbers given
+   *    to this statistician -- stream of numbers treated as a
+   *    population.
+   * <b>Note:</b><br>
+   *   If <CODE>length()</CODE> is zero, then the return from this method
+   *   is <CODE>Double.NaN</CODE>.
+   **/
+   public double stdDev() {
+       if ( this.length() == 0 )
+           return Double.NaN;
+       //Student implementation replace the following return with
+       //appropriate computations and return
+       return 0.0;
+   }
+
+   /**
+   * Create a string representation of this Statistician.<br><br>
+   * <b>Postcondition:</b><br>
+   *   State of the Statistician remains unaltered by this method
+   * @return String
+   *   a human readable representation of the Statistician's internal
+   *   state
+   **/
+   @Override
+   public String toString( ) {
+       StringBuffer sb = new StringBuffer();
+       sb.append("Statistician: \n");
+       sb.append("\tN      = " + this.N + "\n");
+       sb.append("\tsumX   = " + this.sumX + "\n");
+       sb.append("\tsumXsq = " + this.sumXsq + "\n");
+       if ( this.N == 0 ) {
+           sb.append("\tmean       = NOT DEFINED\n");
+           sb.append("\tstd. dev   = NOT DEFINED\n");
+           sb.append("\tsmallest   = NOT DEFINED\n");
+           sb.append("\tlargest    = NOT DEFINED\n");
+       }
+       else {
+           sb.append("\tmean       = " + this.mean()  + "\n");
+           sb.append("\tstd. dev   = " + this.stdDev() + "\n");
+           sb.append("\tsmallest   = " + this.smallest + "\n");
+           sb.append("\tlargest    = " + this.largest + "\n");
+       }
+       return sb.toString();
+   }
+
+   /**
+   * Determine the sum of all the numbers that have been given to this 
+   * Statistician.
+   * @return
+   *   the sum of all the number that have been given to this 
+   *   Statistician since it was initialized or reinitialized <br>
+   * <b>Note:</b><br>
+   *   If the sum exceeds the bounds of double numbers, then the answer
+   *   from this method may be 
+   *   <CODE>Double.POSITIVE_INFINITY</CODE> or
+   *   <CODE>Double.NEGATIVE_INFINITY</CODE>.
+   **/ 
+   public double sumX( ) {
+      // The student's code will replace the following return with the 
+      // appropriate return
+      return 0;
+   }
+
+   /**
+   * Create a new Statistician that behaves as if it were given all the
+   * numbers from two other statisticians.
+   * @param  s1
+   *   the first of two Statisticians
+   * @param s2
+   *   the second of two Statisticians <br>
+   * <b>Precondition:</b><br>
+   *   Neither s1 nor s2 is null.
+   * @return
+   *   a new Statistician that acts as if it were given all the numbers from
+   *   both s1 and s2.
+   * @throws NullPointerException
+   *   Indicates that one or the other or both of the arguments are null.
+   **/   
+   public static Statistician union(Statistician s1, Statistician s2) {
+      if ( s1 == null || s2 == null )
+          throw new NullPointerException("One or both arguments are null");
+
+      Statistician temp = new Statistician();
+      // Student's code to correctly set temp to be union
+
+      return temp;
+   }
+      
+}
+           
