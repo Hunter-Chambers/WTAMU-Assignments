fa58b090ff1e200c5f1fbda1f920566a18329913
hunter-chambers <hrchambers.333@gmail.com>
2020-11-12 12:02:06 -0600

    Finished Lab06/Python

--- CS2336/Lab06/Python/Solution/stack.py
+++ CS2336/Lab06/Python/Solution/stack.py
@@ -3,12 +3,40 @@ from typing import TypeVar
 Stack = TypeVar('Stack')
 
 class stack:
-    '''This Stack is a fixed-sized stack whose
-       capacity is determined by the constructor and for
-       which no capacity adjustments are provided.  The
-       element type of this stack is int'''
+    '''
+    This Stack is a fixed-sized stack whose
+    capacity is determined by the constructor and for
+    which no capacity adjustments are provided.  The
+    element type of this stack is int
+    '''
+    # Class invariant:
+    #   This implementation of a stack mimics that way stacks grow in
+    #   memory of modern day operating systems.  That means that some
+    #   amount of space is allocated arbitrarily -- in this case using
+    #   a Python list with all cells initialized to None.  The list
+    #   is named __data.  An index named __top indicates which
+    #   stack element is at the "top" of the stack if there are any items
+    #   on the stack.  This __top is initially set to the length of the
+    #   __data list which is effectively the "bottom" if the stack.
+    #
+    #   The stack is empty if __top == __capacity
+    #
+    #   push does the following (assuming stack not full):
+    #       __top is decremented
+    #       element to be pushed is copied onto stack at index __top position
+    #
+    #   pop does the following (assuming stack not empty):
+    #       the element at index __top is copied into a variable to be returned
+    #       the reference at index __top is set to None
+    #       __top is incremented
+    #       the copied element is returned to the caller
+    #
+    #   top does the following (assuming stack not empty):
+    #       returns a reference to the top element on the stack
+    #
     def __init__(self, size=1):
-        '''This constructor defines three attributes:
+        '''
+        This constructor defines three attributes:
              (1) __data -- a reference to whatever is chosen
                  to manage the elements on the stack,
              (2) __top -- a reference to the top item of the
@@ -16,68 +44,158 @@ class stack:
              (3) __capacity -- how many elements could potentially
                  be defined on the stack
 
-           Preconditions:  size is an integer and size >= 1
+        Preconditions:  size is an integer and size >= 1
+
+        Raises:  ValueError if size is None
+                 TypeError if size is not int
+                 ValueError if size < 1
         '''
-        # Enforce the Preconditions
+        # STUDENTS Enforce the Preconditions
+        if (size is None or size < 1):
+            raise ValueError("Size must be 1 or greater")
+        if (not isinstance(size, int)):
+            raise TypeError("Size must be an int")
+        # end ifs
+
         self.__data = [None] * size   # creates list of size elements
-        self.__top = -1
+        self.__top  = len(self.__data)
         self.__capacity = size
     # end __init__
 
     def is_empty(self) -> bool:
-        '''returns true if the state of the stack indicates
-        that it is managing no elements'''
-        pass
+        '''
+        returns true if the state of the stack indicates
+        that it is managing no elements; false otherwise
+        '''
+        if (self.__top == self.__capacity):
+            return True
+        # end if
+
+        return False
     # end is_empty
 
+    def is_full(self) -> bool:
+        '''
+        returns true if the state of the stack indicates
+        that the stack is full--the number of elements on
+        the stack equals the stack capacity; false otherwise
+        '''
+        if (self.__top == 0):
+            return True
+        # end if
+
+        return False
+    # end is_full
+
     def push(self, element:int) -> None:
-        ''' alters the state of the stack by copying element
-        onto the stack and adjusting top
+        '''
+        alters the state of the stack by adjusting the top of stack
+        and copying reference to element to the new top
 
         Preconditions:  stack must NOT be full
                         value of element must NOT be None
                         type of element MUST be int
+
+        Raises:  RuntimeError if the stack is full
+                 ValueError if element is None
+                 TypeError if the type of element is not int
         '''
-        pass
+        # STUDENTS Enforce the Preconditions and then implement
+        # according to class invariant
+        if (self.is_full()):
+            raise RuntimeError("Stack is full!")
+        if (element is None):
+            raise ValueError("Element must not be None")
+        if (not isinstance(element, int)):
+            raise TypeError("Element must be an int")
+        # end ifs
+
+        self.__top -= 1
+        self.__data[self.__top] = element
     # end push
 
     def pop(self) -> int:
-        ''' returns a copy of the top element on the stack
-        and ajusts top
+        '''
+        returns a copy of the top element on the stack and adjusts top
 
         Precondition: stack must NOT be empty
+
+        Postcondition: copy of reference to top element returned
+                       and the reference in stack set to None
+
+        Raises:  RuntimeError if stack is empty
        '''
-        pass
+        # STUDENTS Enforce the Preconditions and then implement
+        # according to class invariant
+        if (self.is_empty()):
+            raise RuntimeError("Stack is empty!")
+        # end if
+
+        temp = self.__data[self.__top]
+        self.__top += 1
+        return temp
     # end pop
 
     def top(self) -> int:
-        '''returns a copy of the top element on the stack
+        '''
+        returns a copy of the top element on the stack
         but does NOT adjust top
 
         Precondition:  stack must NOT be empty
+
+        Raises:  RuntimeError if stack is empty
         '''
-        pass
+        # STUDENTS Enforce the Preconditions and then implement
+        # according to class invariant
+        if (self.is_empty()):
+            raise RuntimeError("Stack is empty; has no top value")
+        # end if
+
+        return self.__data[self.__top]
     # end top
 
     def size(self) -> int:
-        '''returns the number of elements on the stack'''
-        pass
+        '''
+        returns the number of elements on the stack
+        '''
+        return self.__capacity - self.__top
     # end size
 
     def reset(self) -> None:
-        '''returns the stack to its state as defined by its
-        constructor -- should do NO data allocation'''
+        '''
+        returns the stack to its state as defined by its
+        constructor -- should do NO data allocation
+
+        Postcondition:  stack in same state as a newly constructed stack
+        '''
         # NOTE:  all cells of the list should be set to None
-        pass
+        for i in range(self.__top, self.__capacity):
+            self.__data[i] = None
+        # end for
+
+        self.__top = self.__capacity
     # end reset
 
     def __str__(self) -> str:
-        '''returns a human-readable representation of the
-        stack to support debugging'''
+        '''
+        returns a human-readable representation of the
+        stack to support debugging
+        '''
         # NOTE:  Use previous code provided to you to generate
         #        a string that is a reasonable, human-readable
         #        representation of the stack -- don't show those
         #        cells of the stack beyond top
-        pass
+        string = ("Capacity: " + str(self.__capacity))
+        string += ("\nSize: " + str(self.size()))
+        string += "\nTop ==> "
+
+        i = self.__top
+        while (i < self.__capacity - 1):
+            string += (str(self.__data[i]) + "\n        ")
+            i += 1
+        # end while
+        string += str(self.__data[i])
+
+        return string
     # end __str__
 # end stack
--- CS2336/Lab06/Python/Solution/towers.py
+++ CS2336/Lab06/Python/Solution/towers.py
@@ -5,15 +5,23 @@ from stack import Stack,stack
 
 def tower_range(aStack : Stack) -> int :
     '''this function is given a stack as an argument with
-    the assumptions:
+       the assumptions:
         (1) the stack contains the height of at least one tower
         (2) any subsequent heights on the stack represent heights
-            of towers to the left of top height
+            of towers to the left of the tower height at top of stack
         (3) the function returns the range of the tower that was
             on top of the stack at entry to this function
         (4) minimum range is 1 as defined in problem statement
     '''
-    pass
+    tower = aStack.pop()
+    t_range = 1
+
+    while ((not aStack.is_empty()) and tower >= aStack.top()):
+        t_range += 1
+        aStack.pop()
+    # end while
+
+    return t_range
 # end range
 
 
@@ -26,18 +34,30 @@ def main():
 
     Data structures needed are:
 
-        H -- a list of N elements
+        height -- a list of N elements
         heightStack -- a stack with capacity of N
     '''
-    heightStack = stack(100)
+    N = int(input())
+    height = [None] * N
+    heightStack = stack(N)
 
-    pass
+    for i in range(N):
+        height[i] = int(input())
+    # end for
 
+    for i in range(N):
+        for j in range(0, i + 1):
+            heightStack.push(height[j])
+        # end for
+        print(tower_range(heightStack), end=' ')
+        #print(heightStack, "\n")
+        heightStack.reset()
+    # end for
+
+    print()
 # end main
 
 if __name__ == "__main__":
     main()
 # end if
-
-
 
